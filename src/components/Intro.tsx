2de3fb498545f7d01016ee7c0bc7c70bc6b8af64
russl8 <russl8@my.yorku.ca>
2023-08-28 15:56:53 -0400

    add  twcss

---
+++ src/components/Intro.tsx
@@ -0,0 +1,12 @@
+import Typewriter from 'react-ts-typewriter';
+
+const Intro = () => {
+    return (
+        <div>
+            <Typewriter text="Hi, Russell Here." speed={120}/>
+
+        </div>
+    );
+}
+
+export default Intro;
