850ddb9af51202a5fe72d3cf1bf6b2eddbd02557
russl8 <russl8@my.yorku.ca>
2023-08-28 16:06:24 -0400

    font

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,12 @@
 import Typewriter from 'react-ts-typewriter';
 
+//https://github.com/gerardmarquinarubio/ReactTypewriter
 const Intro = () => {
     return (
         <div>
-            <Typewriter text="Hi, Russell Here." speed={120}/>
-
+            <h1 className='text-3xl font-bold underline'>
+                <Typewriter text="Hi, Russell Here." speed={120} />
+            </h1>
         </div>
     );
 }
