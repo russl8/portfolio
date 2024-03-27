6d639c6a6ae95dd8a3bfb3f3d56902662f85cab7
russl8 <russl8@my.yorku.ca>
2024-03-27 02:39:55 -0400

    navbar zindex fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,22 +5,24 @@ const links = [
     <SidebarLink href="#intro" text="home" />,
     <SidebarLink href="#about" text="about" />,
     <SidebarLink href="#projects" text="my projects" />
-]
+];
+
 const Navbar = () => {
     return (
-        <FadeInSection delay={`100ms`}>
-            <div className="w-full flex justify-center z-1000 items-center mt-4 px-10 sm:hidden">
-                <div className=" fixed  w-[450px] py-2 text-color bg-[#000000] rounded-lg flex justify-around items-center mt-8 mx-4">
-                    {links.map((link, i) => (
-                        <div className="">
-                            {link}
-                        </div>
-                    ))}
+        <div className="fixed  z-50 w-full text-color">
+            <FadeInSection delay={`100ms`}>
+                <div className=" w-[450px]flex justify-center items-center mt-4 px-10 sm:hidden">
+                    <div className="w-450 py-2 bg-[#000000] rounded-lg flex justify-around items-center mt-8 mx-4">
+                        {links.map((link, i) => (
+                            <div key={i} className="mx-2">
+                                {link}
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </FadeInSection>
-
+            </FadeInSection>
+        </div>
     );
-}
+};
 
 export default Navbar;
